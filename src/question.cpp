9057368e444cd85d583e02fffd4d9e435ca3a075
kilometers <kilo@meters.sh>
2022-03-26 11:27:49 -0700

    fix

--- src/question.cpp
+++ src/question.cpp
@@ -1,5 +1,3 @@
-// We need to develop our inheritence tree before we can start any work
-
 #include <iostream>
 
 #include "question.h"
@@ -7,43 +5,41 @@
 using namespace std;
 
 string to_string(QuestionType type) {
-    switch (type) {
-        case EMPATHY:
-            return "empathy";
-        case COOPORATION:
-            return "cooporation";
-        case COMMUNICATION:
-            return "communication";
-        case LISTENING:
-            return "listening";
-        case NONVERBAL:
-            return "nonverbal";
-    };
+  switch (type) {
+  case EMPATHY:
+    return "empathy";
+  case COOPORATION:
+    return "cooporation";
+  case COMMUNICATION:
+    return "communication";
+  case LISTENING:
+    return "listening";
+  case NONVERBAL:
+    return "nonverbal";
+  };
 }
 
 Question::Question(string question, QuestionType qtype) {
-    this->question = question;
-    this->qtype = qtype;
+  this->question = question;
+  this->qtype = qtype;
 }
 
 double Question::prompt() {
-    cout << "\n\033[1m" << question << "\033[0m [" << to_string(qtype) << "]\n";
+  cout << "\n\033[1m" << question << "\033[0m [" << to_string(qtype) << "]\n";
 
-    int value = 1;
-    do {
-        if(cin.fail() || value > 5 || value < 1){
-            cout << "\nInput must be an integer between 1 and 5. Try again.\n"; 
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        }
+  int value = 1;
+  do {
+    if (cin.fail() || value > 5 || value < 1) {
+      cout << "\nInput must be an integer between 1 and 5. Try again.\n";
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
 
-        cout << "[1,2,3,4,5]: ";
-        cin >> value;
-    } while(cin.fail() || value > 5 || value < 1);
+    cout << "[1,2,3,4,5]: ";
+    cin >> value;
+  } while (cin.fail() || value > 5 || value < 1);
 
-    return (double) value;
+  return (double)value;
 }
 
-QuestionType Question::get_type() {
-    return qtype;
-}
+QuestionType Question::get_type() { return qtype; }
